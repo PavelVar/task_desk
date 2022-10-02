dd8025affd16c167829fb187e423dcfd61a1497d
Pavel Varyvonchyk <pavel_varyvonchyk@epam.com>
2022-10-02 13:23:54 +0300

    fix move, add, move, del works

--- src/js/deletes.js
+++ src/js/deletes.js
@@ -8,23 +8,28 @@ export function moveTask(event) {
   let task = new Object();
   let elID =
     event.target.closest(".task-wrap").firstChild.firstChild.textContent;
-  for (i of document.querySelectorAll(".data-task_text")) {
-    if ((i.dataset.elid = elID)) {
-      task.text = i.textContent;
+
+  let items = document.querySelectorAll(".data-task_text");
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].dataset.elid == elID) {
+      task.text = items[i].textContent;
     }
   }
+
   for (i of document.querySelectorAll(".task-date")) {
-    if ((i.dataset.elidDate = elID)) {
+    if (i.dataset.elidDate == elID) {
       task.date = i.textContent;
     }
   }
+
   for (i of document.querySelectorAll(".task-prior")) {
-    if ((i.dataset.elidPrior = elID)) {
+    if (i.dataset.elidPrior == elID) {
       task.priority = i.textContent;
     }
   }
+
   for (i of document.querySelectorAll(".responsible")) {
-    if ((i.dataset.elidResp = elID)) {
+    if (i.dataset.elidResp == elID) {
       task.responsible = i.textContent;
     }
   }
